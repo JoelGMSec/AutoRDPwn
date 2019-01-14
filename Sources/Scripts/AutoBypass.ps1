09ae40952f4f13e5a3f191469eed41c8604a1b8c
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2019-01-14 08:46:12 +0100

    Update AutoBypass.ps1

--- Sources/Scripts/AutoBypass.ps1
+++ Sources/Scripts/AutoBypass.ps1
@@ -29,6 +29,50 @@ REG ADD "HKCU\Console" /v ColorTable13 /t REG_DWORD /d 10354868 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable14 /t REG_DWORD /d 10875385 /f 2>&1> $null
 REG ADD "HKCU\Console" /v ColorTable15 /t REG_DWORD /d 15921906 /f 2>&1> $null
 
+$code = @'
+using System;
+using System.Runtime.InteropServices;
+
+namespace CloseButtonToggle {
+
+ internal static class WinAPI {
+   [DllImport("kernel32.dll")]
+   internal static extern IntPtr GetConsoleWindow();
+
+   [DllImport("user32.dll")]
+   [return: MarshalAs(UnmanagedType.Bool)]
+   internal static extern bool DeleteMenu(IntPtr hMenu,
+                          uint uPosition, uint uFlags);
+
+   [DllImport("user32.dll")]
+   [return: MarshalAs(UnmanagedType.Bool)]
+   internal static extern bool DrawMenuBar(IntPtr hWnd);
+
+   [DllImport("user32.dll")]
+   internal static extern IntPtr GetSystemMenu(IntPtr hWnd,
+              [MarshalAs(UnmanagedType.Bool)]bool bRevert);
+
+   const uint SC_CLOSE     = 0xf060;
+   const uint MF_BYCOMMAND = 0;
+
+   internal static void ChangeCurrentState(bool state) {
+     IntPtr hMenu = GetSystemMenu(GetConsoleWindow(), state);
+     DeleteMenu(hMenu, SC_CLOSE, MF_BYCOMMAND);
+     DrawMenuBar(GetConsoleWindow());
+   }
+ }
+
+ public static class Status {
+   public static void Disable() {
+     WinAPI.ChangeCurrentState(false); //its 'true' if need to enable
+   }
+ }
+}
+'@
+
+Add-Type $code
+[CloseButtonToggle.Status]::Disable()
+
 function Bypass-UAC {
 Param(
 [Parameter(Mandatory = $true,  Position = 0)]
