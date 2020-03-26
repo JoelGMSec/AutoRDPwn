88b1ddff6feeee2af22e8182844d64976714ba28
JoelGMSec <34335312+JoelGMSec@users.noreply.github.com>
2020-03-26 10:45:42 +0100

    Update RDP-Caching.ps1

--- Resources/Scripts/RDP-Caching.ps1
+++ Resources/Scripts/RDP-Caching.ps1
@@ -48,7 +48,6 @@ while ($bytesRead -eq $bufSize)
         }
         $imgNoText=($imgNo).ToString("0000")
         $bmBM.Save("$workingDir\$imgNamePrefix.$imgNoText.png")
+        Write-Progress -Activity "Completing row" -Completed
+        Write-Progress -Activity "Analyzing cache file" -Completed
     }
-    
-Write-Progress -Activity "Analyzing cache file" -Completed
-Write-Progress -Activity "Completing row" -Completed
